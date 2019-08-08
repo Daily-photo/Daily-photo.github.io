5bf3f93e08117b418d8dc57d6e384340c7c1750b
Daily-photo <53793406+Daily-photo@users.noreply.github.com>
2019-08-08 22:22:38 +0700

    Update Java.js

--- styles/Java.js
+++ styles/Java.js
@@ -1,13 +1,15 @@
-
-
-function openbox(id){
-    display = document.getElementById(id).style.display;
-
-    if(display=='none'){
-       document.getElementById(id).style.display='block';
-    }else{
-       document.getElementById(id).style.display='none';
-    }
+function openbox(box){
+	display=document.getElementById('box').style.display;
+	if (display =="block")
+	{
+		document.getElementById('box').style.display="none";
+	} 
+	else
+	{
+		document.getElementById('box').style.display="block";
+	}
+	 
+	
 }
 
 
